dee99f0e9e8feba195a0fc9f73023fbd433752ed
DrobyshevIvan <102222211+DrobyshevIvan@users.noreply.github.com>
2025-07-14 13:11:30 +0300

    Added unit of work

---
+++ Backend/HealthBeside/HealthBeside.Infrastructure/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,29 @@
+﻿using HealthBeside.Domain.Interfaces;
+
+namespace HealthBeside.Infrastructure.UnitOfWork;
+
+public class UnitOfWork : IUnitOfWork
+{
+    private readonly AppDbContext _context;
+    public IForumCommentRepository ForumCommentRepository { get; }
+    public IForumPostRepository ForumPostRepository { get; }
+
+    public UnitOfWork(AppDbContext context,
+        IForumCommentRepository forumCommentRepository,
+        IForumPostRepository forumPostRepository)
+    {
+        _context = context;
+        ForumCommentRepository = forumCommentRepository;
+        ForumPostRepository = forumPostRepository;
+    }
+
+    public async Task SaveChangesAsync(CancellationToken cancellationToken = default)
+    {
+        await _context.SaveChangesAsync(cancellationToken);
+    }
+
+    public void Dispose()
+    {
+        _context.Dispose();
+    }
+}
